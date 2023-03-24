5864ea7afbe5ad1df8f7fd714654543e8647ea02
igjorque <ignacio.jor.fer@gmail.com>
2023-03-24 12:20:51 +0100

    Logger arreglado

--- OTOI_ADD/Program.cs
+++ OTOI_ADD/Program.cs
@@ -20,10 +20,22 @@ namespace OTOI_ADD
         [STAThread]
         static void Main()
         {
+            // Create C:/OTOI_ADD/log4net.config if doesn't exist
+            if (!Directory.Exists("C:\\OTOI_ADD"))
+            {
+                Directory.CreateDirectory("C:\\OTOI_ADD");
+            }
+            if (!File.Exists("C:\\OTOI_ADD\\log4net.config"))
+            {
+                InitConfig();
+            }
+
             // Initialize logger configuration
             var logRepository = LogManager.GetRepository(Assembly.GetEntryAssembly());
-            XmlConfigurator.Configure(logRepository, new FileInfo("log4net.config"));
-            logger = log4net.LogManager.GetLogger(MethodBase.GetCurrentMethod().DeclaringType);
+            // Debug config
+            XmlConfigurator.Configure(logRepository, new FileInfo("C:\\OTOI_ADD\\log4net.config"));
+
+            logger = LogManager.GetLogger(MethodBase.GetCurrentMethod().DeclaringType);
 
             logger.Info("Application - Loading");
 
@@ -33,6 +45,27 @@ namespace OTOI_ADD
             Application.Run(new Main());
 
             logger.Info("Application - Shutdown");
+        }
+
+        private static void InitConfig()
+        {
+            using FileStream fs = File.Open("C:\\OTOI_ADD\\log4net.config", FileMode.OpenOrCreate);
+            using StreamWriter sw = new(fs);
+            sw.WriteLine("<?xml version=\"1.0\" encoding=\"utf-8\" ?>");
+            sw.WriteLine("<configuration>");
+            sw.WriteLine("  <log4net>");
+            sw.WriteLine("      <root>");
+            sw.WriteLine("          <level value=\"ALL\" />");
+            sw.WriteLine("          <appender-ref ref=\"all_logs_file\" />");
+            sw.WriteLine("      </root>");
+            sw.WriteLine("      <appender name=\"all_logs_file\" type=\"log4net.Appender.FileAppender\">");
+            sw.WriteLine("          <file value=\"C:\\\\OTOI_ADD\\\\log.log\" />");
+            sw.WriteLine("          <layout type=\"log4net.Layout.PatternLayout\">");
+            sw.WriteLine("              <conversionPattern value=\"[%date] > [%level] > [%logger] > [%message]%newline\" />");
+            sw.WriteLine("          </layout>");
+            sw.WriteLine("      </appender>");
+            sw.WriteLine("  </log4net>");
+            sw.WriteLine("</configuration>");
         }
     }
 }
